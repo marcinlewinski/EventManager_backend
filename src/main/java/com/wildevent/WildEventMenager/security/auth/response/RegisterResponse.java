c15a546a913f3b8f5409c2c191c29ad7a5615239
Michał Zuzak <michalzuzak.2@gmail.com>
2023-10-28 15:29:57 +0200

    RegisterResponse implemented

--- src/main/java/com/wildevent/WildEventMenager/security/auth/response/RegisterResponse.java
+++ src/main/java/com/wildevent/WildEventMenager/security/auth/response/RegisterResponse.java
@@ -1,7 +1,5 @@
 package com.wildevent.WildEventMenager.security.auth.response;
 
-import com.wildevent.WildEventMenager.location.model.Location;
-import com.wildevent.WildEventMenager.role.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +16,6 @@ public class RegisterResponse {
     private String name;
     private String phone;
     private String email;
-    private Set<Role> roles;
-    private List<Location> locations;
+    private Set<String> roles;
+    private List<String> locations;
 }
